f6fe815e4c1d131969aed8a8b2ee6e183f21cf9f
Krystian Maccs <krystianmaccs@gmail.com>
2023-04-17 23:41:20 +0100

    making changes to the signals

--- apps/utils/apps.py
+++ apps/utils/apps.py
@@ -3,6 +3,7 @@ from django.apps import AppConfig
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from django.db.models import Model
+from django.contrib.contenttypes.models import ContentType
 from pymongo import MongoClient
 from decouple import config
 
@@ -12,7 +13,7 @@ class UtilsConfig(AppConfig):
 
     def ready(self):
         for model in self.get_models():
-            @receiver(post_save, sender=Model)
+            @receiver(post_save, sender=ContentType(model=model))
             def save_to_mongodb_signal(sender, instance, **kwargs):
                 client = MongoClient(host=config("MONGO_HOST", cast=str), port=config("MONGO_PORT", cast=int))
                 db = client[config("MONGO_NAME")]
@@ -23,7 +24,7 @@ class UtilsConfig(AppConfig):
                 mongo_object = json.loads(instance.to_json())
                 mongo_collection.replace_one({'_id': mongo_object['_id']}, mongo_object, upsert=True)
 
-            @receiver(post_save, sender=Model)
+            @receiver(post_save, sender=ContentType(model=model))
             def update_to_mongodb_signal(sender, instance, **kwargs):
                 # Connect to MongoDB using pymongo
                 client = MongoClient(host=config("MONGO_HOST", cast=str), port=config("MONGO_PORT", cast=int))
@@ -35,7 +36,7 @@ class UtilsConfig(AppConfig):
                 mongo_object = json.loads(instance.to_json())
                 mongo_collection.update_one({'_id': mongo_object['_id']}, {'$set': mongo_object}, upsert=True)
 
-            @receiver(post_delete, sender=Model)
+            @receiver(post_save, sender=ContentType(model=model))
             def pre_delete_from_mongodb_signal(sender, instance, **kwargs):
                 # Connect to MongoDB using pymongo
                 client = MongoClient(host=config("MONGO_HOST", cast=str), port=config("MONGO_PORT", cast=int))
